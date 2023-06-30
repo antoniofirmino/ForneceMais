9f51786d388f4c2aa87b70f6e82d15c1e90ce3fe
Renan Tavares <renan_velox@hotmail.com>
2023-06-30 09:16:18 -0300

    criando a scroll

--- beluga/ViewController.swift
+++ beluga/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewHeader: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let border = CALayer()
+        border.backgroundColor = UIColor.blue.cgColor
+        border.frame = CGRect(x: 0, y: viewHeader.frame.size.height - 1, width: viewHeader.frame.size.width, height: 1)
+        viewHeader.layer.addSublayer(border)
+        
+        view.addSubview(viewHeader)
     }
+    
+    
 
 
 }
+
 
