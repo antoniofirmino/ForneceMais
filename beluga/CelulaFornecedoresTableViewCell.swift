4d9edda248e61ce3bffb371153e8e93993fddd4e
Ricardo de Aagostini <ricardoagneto@gmail.com>
2023-07-06 11:35:26 -0300

    tela fornecedores ricardo

---
+++ beluga/CelulaFornecedoresTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CelulaFornecedoresTableViewCell.swift
+//  beluga
+//
+//  Created by Ricardo de Agostini Neto on 05/07/23.
+//
+
+import UIKit
+
+class CelulaFornecedoresTableViewCell: UITableViewCell {
+
+ 
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
