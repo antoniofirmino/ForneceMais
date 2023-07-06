ab9365931b3eeb9490fe3fd2fb0acc27e620a29f
Antonio Marcos <87496576+antoniofirmino@users.noreply.github.com>
2023-07-06 10:12:08 -0300

    Iniciando com cell

---
+++ beluga/SearchCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCollectionViewCell.swift
+//  beluga
+//
+//  Created by Antonio Marcos on 06/07/23.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
