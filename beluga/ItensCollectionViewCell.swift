1d796a1151dc8f2a4887cd345173e0999c531f37
Renan Tavares <renan_velox@hotmail.com>
2023-07-06 10:31:16 -0300

    criando a tela principal

---
+++ beluga/ItensCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItensCollectionViewCell.swift
+//  beluga
+//
+//  Created by Renan Tavares on 04/07/23.
+//
+
+import UIKit
+
+class ItensCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var nichoCollectionCell: UILabel!
+    @IBOutlet weak var starsCollectionCell: UIImageView!
+    @IBOutlet weak var forncedorNameCollectionCell: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        nichoCollectionCell.text = "alimentação"
+        starsCollectionCell.image = UIImage(named: "Frame 21")
+        forncedorNameCollectionCell.text = "Fornecedor 1"
+    }
+}
